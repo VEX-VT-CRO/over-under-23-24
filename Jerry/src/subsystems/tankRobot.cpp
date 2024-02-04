c1774017850048d1ee1153f8b429710ccdff9d55
msannikov03 <msannikov03@gmail.com>
2024-02-04 09:33:15 -0500

    Temporary

--- Jerry/src/subsystems/tankRobot.cpp
+++ Jerry/src/subsystems/tankRobot.cpp
@@ -18,78 +18,84 @@ void TankRobot::pollController(bool dualDriver)
 {
     static bool manualAim = false;
     constexpr int TURRET_SPEED = 6000;
+    static bool rapidshootmode = false;
     drivetrain.tankControl(driver);
 
-    if(driver.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT))
-    {
-        ri.spin(-ri.STANDARD_MV);
-    }
-    else if(driver.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
-    {
-        ri.spin(ri.STANDARD_MV);
-    }
-    else
-    {
-        ri.spin(0);
-    }
-
-    if(driver.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R2))
-    {
-        if (!catapult->shoot_ready){
-            catapult->charge_state = true;
-            pros::delay(20);
-            turret->rotateback();
-        }    
-        else{
-            catapult->shoot_state = true; 
-            catapult->shoot_ready = false;
-        }      
-    }
-
-    if(catapult->free_move)
-        if(driver.get_digital(pros::E_CONTROLLER_DIGITAL_Y))
-            catapult->spin(-6000);
-        else
-            catapult->spin(0);
-
-    if(driver.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1))
-    {
-        spool->moveTo(SpoolPosition::RETRACTED);
-    }
-
-    if(driver.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_LEFT)){
-        catapult->free_move = !catapult->free_move;
-    }
-
-    if(driver.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1)){
-            turret->updatePosition();
-    }
-    
-    if(driver.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)){
-        turret->reset_angles();
-    }
-    manualAim = (driver.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP)) ? !manualAim : manualAim;
-    
-    if(dualDriver){
-        int turretDirection = partner.get_digital(pros::E_CONTROLLER_DIGITAL_L1) - partner.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
-        turret->turnVoltage(TURRET_SPEED * turretDirection);
-        if(partner.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)){
-            turret->reset_angles();
+    if(!rapidshootmode){
+        if(driver.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT))
+        {
+            ri.spin(-ri.STANDARD_MV);
         }
+        else if(driver.get_digital(pros::E_CONTROLLER_DIGITAL_L2))
+        {
+            ri.spin(ri.STANDARD_MV);
+        }
+        else
+        {
+            ri.spin(0);
+        }
+
+        if(driver.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R2))
+        {
+            if (!catapult->shoot_ready){
+                catapult->charge_state = true;
+                pros::delay(20);
+                turret->rotateback();
+            }    
+            else{
+                catapult->shoot_state = true; 
+                catapult->shoot_ready = false;
+            }      
+        }
+
         if(catapult->free_move)
-            if(partner.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
+            if(driver.get_digital(pros::E_CONTROLLER_DIGITAL_Y))
                 catapult->spin(-6000);
             else
                 catapult->spin(0);
-        if(partner.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1)){
-            catapult->free_move = !catapult->free_move;
-    }        
-    }
-    if(manualAim)
-    {
-        int turretDirection = driver.get_digital(pros::E_CONTROLLER_DIGITAL_L1) - driver.get_digital(pros::E_CONTROLLER_DIGITAL_R1); 
-        turret->turnVoltage(TURRET_SPEED * turretDirection);
-    }
 
+        if(driver.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1))
+        {
+            spool->moveTo(SpoolPosition::RETRACTED);
+        }
+
+        if(driver.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_LEFT)){
+            catapult->free_move = !catapult->free_move;
+        }
+
+        if(driver.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1)){
+                turret->updatePosition();
+        }
+        
+        if(driver.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)){
+            turret->reset_angles();
+        }
+        manualAim = (driver.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP)) ? !manualAim : manualAim;
+        
+        if(dualDriver){
+            int turretDirection = partner.get_digital(pros::E_CONTROLLER_DIGITAL_L1) - partner.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
+            turret->turnVoltage(TURRET_SPEED * turretDirection);
+            if(partner.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)){
+                turret->reset_angles();
+            }
+            if(catapult->free_move)
+                if(partner.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
+                    catapult->spin(-6000);
+                else
+                    catapult->spin(0);
+            if(partner.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1)){
+                catapult->free_move = !catapult->free_move;
+        }        
+        }
+        if(manualAim)
+        {
+            int turretDirection = driver.get_digital(pros::E_CONTROLLER_DIGITAL_L1) - driver.get_digital(pros::E_CONTROLLER_DIGITAL_R1); 
+            turret->turnVoltage(TURRET_SPEED * turretDirection);
+        }
+    }
+    else{
+        
+    }    
+    rapidshootmode = (driver.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_X)) ? !rapidshootmode : rapidshootmode;
     pros::lcd::clear();
 }
